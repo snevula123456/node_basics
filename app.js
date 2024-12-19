66bd81eb950757ac45726b6c5c7a498577d98376
snevula123456 <snevula123@gmail.com>
2024-12-19 18:39:44 +0530

    Check 3

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req,res)=> {
-    console.log("Checking the git items")
-    res.send("Hello World!")
-})
+app.get("/", (req, res) => {
+  console.log("Checking the git items");
+  console.log("Checking the git items 2");
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app Running on the port ${port}`)
-})
+  console.log(`Example app Running on the port ${port}`);
+});
